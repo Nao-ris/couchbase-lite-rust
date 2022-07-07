276994f8f56172dad8fdb0226ee6c12a2f313915
M MatthieuClochardDoctolib <matthieu.clochard@doctolib.com>
2022-07-07 14:37:20 +0200

    Add tests for Document Database impl

--- tests/document_tests.rs
+++ tests/document_tests.rs
@@ -59,13 +59,17 @@ fn document_sequence() {
 #[test]
 fn document_properties() {
     let mut document = Document::new();
-    let mut initial_properties = MutableDict::new();
-    initial_properties.at("foo").put_bool(false);
-    initial_properties.at("bar").put_bool(true);
-    document.set_properties(initial_properties);
-    let mut set_properties = document.mutable_properties();
-    set_properties.at("baz").put_bool(true);
-    set_properties.at("foo").put_bool(true);
+    {
+        let mut initial_properties = MutableDict::new();
+        initial_properties.at("foo").put_bool(false);
+        initial_properties.at("bar").put_bool(true);
+        document.set_properties(initial_properties);
+    }
+    {
+        let mut set_properties = document.mutable_properties();
+        set_properties.at("baz").put_bool(true);
+        set_properties.at("foo").put_bool(true);
+    }
     let final_properties = document.properties();
     assert_eq!(final_properties.count(), 3);
     assert_eq!(final_properties.get("foo").as_bool_or_false(), true);
@@ -87,24 +91,68 @@ fn document_properties_as_json() {
 }
 
 #[test]
-fn save_document() {
+fn database_get_document() {
+    utils::with_db(|db| {
+        let mut document = Document::new();
+        db.save_document(&mut document, ConcurrencyControl::FailOnConflict).expect("save_document");
+        let got_document = db.get_document(document.id());
+        assert!(got_document.is_ok());
+        assert_eq!(got_document.unwrap().id(), document.id());
+        let no_document = db.get_document("");
+        assert!(no_document.is_err());
+    });
+}
+
+#[test]
+fn database_save_document() {
     utils::with_db(|db| {
         {
-            let mut doc = Document::new_with_id("foo");
-            let mut props = doc.mutable_properties();
-            props.at("i").put_i64(1234);
-            props.at("s").put_string("Hello World!");
-
-            db.save_document(&mut doc, ConcurrencyControl::FailOnConflict).expect("save");
+            let mut document = Document::new_with_id("foo");
+            db.save_document(&mut document, ConcurrencyControl::FailOnConflict).expect("save_document");
         }
         {
-            let doc = db.get_document("foo").expect("reload document");
-            let props = doc.properties();
-            verbose!("Blah blah blah");
-            info!("Interesting: {} = {}", 2+2, 4);
-            warn!("This is a warning");
-            error!("Oh no, props = {}", props);
-            assert_eq!(props.to_json(), r#"{"i":1234,"s":"Hello World!"}"#);
+            let document = db.get_document("foo");
+            assert!(document.is_ok());
         }
+    });
+}
+
+#[test]
+fn database_save_document_resolving() {
+    utils::with_db(|_db| {
+        // TODO
+    });
+}
+
+#[test]
+fn database_purge_document_by_id() {
+    utils::with_db(|db| {
+        {
+            let mut document = Document::new_with_id("foo");
+            db.save_document(&mut document, ConcurrencyControl::FailOnConflict).expect("save_document");
+        }
+        {
+            db.purge_document_by_id("foo").expect("purge_document_by_id");
+        }
+        {
+            let document = db.get_document("foo");
+            assert!(document.is_err());
+        }
+    });
+}
+
+#[test]
+fn database_document_expiration() {
+    utils::with_db(|db| {
+        {
+            let mut document = Document::new_with_id("foo");
+            db.save_document(&mut document, ConcurrencyControl::FailOnConflict).expect("save_document");
+        }
+        let initial_expiration = db.document_expiration("foo").expect("document_expiration");
+        assert!(initial_expiration.is_none());
+        db.set_document_expiration("foo", Some(Timestamp(1000000000))).expect("set_document_expiration");
+        let set_expiration = db.document_expiration("foo").expect("document_expiration");
+        assert!(set_expiration.is_some());
+        assert_eq!(set_expiration.unwrap().0, 1000000000);
     });
 }
