c10d9ef8dd70c90b21b386ef40bc4ea016cb43f5
Adrien Jeser <adrien.jeser@doctolib.com>
2022-07-26 18:03:05 +0200

    ci without san

--- tests/database_tests.rs
+++ tests/database_tests.rs
@@ -32,50 +32,44 @@ lazy_static! {
     static ref DOCUMENT_DETECTED: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
 }
 
+#[test]
+fn in_transaction() {
+    utils::with_db(|db| {
+        let result = db.in_transaction(|db| {
+            let mut doc = Document::new_with_id("document");
+            db.save_document_with_concurency_control(&mut doc, ConcurrencyControl::LastWriteWins)
+                .unwrap();
+            Ok("document".to_string())
+        });
+
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), "document");
+
+        let result = db.in_transaction(|db| -> Result<String> {
+            let mut doc = Document::new_with_id("document_error");
+            db.save_document_with_concurency_control(&mut doc, ConcurrencyControl::LastWriteWins)
+                .unwrap();
+            Err(couchbase_lite::Error::default())
+        });
+
+        assert!(result.is_err());
+
+        assert!(db.get_document("document").is_ok());
+        assert!(db.get_document("document_error").is_err());
+    });
+}
+
+#[test]
+fn db_properties() {
+    utils::with_db(|db| {
+        assert_eq!(db.name(), utils::DB_NAME);
+        assert_eq!(db.count(), 0);
+    });
+}
+
 #[cfg(feature = "concurrency-test")]
 mod concurrency {
     use super::*;
-
-    #[test]
-    fn in_transaction() {
-        utils::with_db(|db| {
-            let result = db.in_transaction(|db| {
-                let mut doc = Document::new_with_id("document");
-                db.save_document_with_concurency_control(
-                    &mut doc,
-                    ConcurrencyControl::LastWriteWins,
-                )
-                .unwrap();
-                Ok("document".to_string())
-            });
-
-            assert!(result.is_ok());
-            assert_eq!(result.unwrap(), "document");
-
-            let result = db.in_transaction(|db| -> Result<String> {
-                let mut doc = Document::new_with_id("document_error");
-                db.save_document_with_concurency_control(
-                    &mut doc,
-                    ConcurrencyControl::LastWriteWins,
-                )
-                .unwrap();
-                Err(couchbase_lite::Error::default())
-            });
-
-            assert!(result.is_err());
-
-            assert!(db.get_document("document").is_ok());
-            assert!(db.get_document("document_error").is_err());
-        });
-    }
-
-    #[test]
-    fn db_properties() {
-        utils::with_db(|db| {
-            assert_eq!(db.name(), utils::DB_NAME);
-            assert_eq!(db.count(), 0);
-        });
-    }
 
     #[test]
     fn add_listener() {
