5ffc6638e327982321b0f5f3678ea83cb158011f
MatthieuClochardDoctolib <matthieu.clochard@doctolib.com>
2022-08-12 16:56:37 +0200

    Remove clang configuration from build.rs

--- build.rs
+++ build.rs
@@ -28,48 +28,19 @@ use std::env;
 use std::fs;
 use std::path::PathBuf;
 
-// Where to find the Couchbase Lite headers and library:    //TODO: Make this easily configurable
 static CBL_INCLUDE_DIR: &str = "libcblite-3.0.1/include";
 static CBL_LIB_DIR: &str = "libcblite-3.0.1/lib";
 
 #[cfg(target_os = "macos")]
 static CBL_LIB_FILENAME: &str = "libcblite.dylib";
-
 #[cfg(target_os = "linux")]
 static CBL_LIB_FILENAME: &str = "libcblite.so";
-
-#[cfg(target_os = "windows")]
-static CBL_LIB_FILENAME: &str = "cblite.dll";
-
-// Where to find Clang and LLVM libraries:
-#[cfg(target_os = "macos")]
-static DEFAULT_LIBCLANG_PATH: &str = "/usr/local/Cellar/llvm/12.0.1/lib";
-
-#[cfg(target_os = "linux")]
-static DEFAULT_LIBCLANG_PATH: &str = "/usr/lib/clang/10/lib/";
-
-#[cfg(target_os = "windows")]
-static DEFAULT_LIBCLANG_PATH: &str = "/usr/lib/clang/10/lib/";
-
-static STATIC_LINK_CBL: bool = false;
-static CBL_SRC_DIR: &str = "../../CBL_C";
+#[cfg(target_os = "win32")]
+static CBL_LIB_FILENAME: &str = "cblite.ddl";
 
 fn main() {
-    // Set LIBCLANG_PATH environment variable if it's not already set:
-    if env::var("LIBCLANG_PATH").is_err() {
-        env::set_var("LIBCLANG_PATH", DEFAULT_LIBCLANG_PATH);
-        println!("cargo:rustc-env=LIBCLANG_PATH={}", DEFAULT_LIBCLANG_PATH);
-    }
-
-    let sdk_root = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.3.sdk";
-
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate bindings for.
         .header("src/wrapper.h")
-        //.header("libcblite-3.0.1/include/cbl/CouchbaseLite.h")
         // C '#include' search paths:
         .clang_arg(format!("-I{}", CBL_INCLUDE_DIR))
         // Which symbols to generate bindings for:
@@ -80,7 +51,6 @@ fn main() {
         .whitelist_function("CBL.*")
         .whitelist_function("_?FL.*")
         .no_copy("FLSliceResult")
-        .clang_arg(format!("-isysroot{}", sdk_root))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -96,63 +66,14 @@ fn main() {
         .expect("Couldn't write bindings!");
 
     // Tell cargo to tell rustc to link the cblite library.
-    if STATIC_LINK_CBL {
-        // Link against the CBL-C and LiteCore static libraries for maximal efficienty.
-        // This assumes that a checkout of couchbase-lite-C exists at CBL_SRC_DIR
-        // and has been built with CMake.
-        // TODO: Currently, on Mac OS this requires manual processing of the static libraries
-        //       because Rust can't link with fat libraries. Thus all the libraries linked below
-        //       had to be thinned with e.g. `libXXX.a -thin x86_64 -output libXXX-x86.a`.
-        let cblite_src_path = PathBuf::from(CBL_SRC_DIR);
-        let root = cblite_src_path.to_str().unwrap();
-
-        println!("cargo:rustc-link-search={}/build_cmake", root);
-        println!(
-            "cargo:rustc-link-search={}/build_cmake/vendor/couchbase-lite-core",
-            root
-        );
-        println!(
-            "cargo:rustc-link-search={}/build_cmake/vendor/couchbase-lite-core/Networking/BLIP",
-            root
-        );
-        println!(
-            "cargo:rustc-link-search={}/build_cmake/vendor/couchbase-lite-core/vendor/fleece",
-            root
-        );
-        println!("cargo:rustc-link-search={}/build_cmake/vendor/couchbase-lite-core/vendor/mbedtls/library", root);
-        println!("cargo:rustc-link-search={}/build_cmake/vendor/couchbase-lite-core/vendor/mbedtls/crypto/library", root);
-        println!("cargo:rustc-link-search={}/build_cmake/vendor/couchbase-lite-core/vendor/sqlite3-unicodesn", root);
-
-        println!("cargo:rustc-link-lib=static=cblite-static-x86");
-        println!("cargo:rustc-link-lib=static=liteCoreStatic-x86");
-        println!("cargo:rustc-link-lib=static=liteCoreWebSocket-x86");
-        println!("cargo:rustc-link-lib=static=BLIPStatic-x86");
-        println!("cargo:rustc-link-lib=static=FleeceStatic-x86");
-        println!("cargo:rustc-link-lib=static=CouchbaseSqlite3-x86");
-        println!("cargo:rustc-link-lib=static=SQLite3_UnicodeSN-x86");
-        println!("cargo:rustc-link-lib=static=mbedcrypto-x86");
-        println!("cargo:rustc-link-lib=static=mbedtls-x86");
-        println!("cargo:rustc-link-lib=static=mbedx509-x86");
-
-        println!("cargo:rustc-link-lib=c++");
-        println!("cargo:rustc-link-lib=z");
-
-        // TODO: This only applies to Apple platforms:
-        println!("cargo:rustc-link-lib=framework=CoreFoundation");
-        println!("cargo:rustc-link-lib=framework=Foundation");
-        println!("cargo:rustc-link-lib=framework=CFNetwork");
-        println!("cargo:rustc-link-lib=framework=Security");
-        println!("cargo:rustc-link-lib=framework=SystemConfiguration");
-    } else {
-        // Link against and copy the CBL dynamic library:
-        let src = PathBuf::from(CBL_LIB_DIR).join(CBL_LIB_FILENAME);
-        let dst = out_dir.join(CBL_LIB_FILENAME);
-        println!("cargo:rerun-if-changed={}", src.to_str().unwrap());
-        fs::copy(src, dst).expect("copy dylib");
-        // Tell rustc to link it:
-        println!("cargo:rustc-link-search={}", out_dir.to_str().unwrap());
-        println!("cargo:rustc-link-lib=dylib=cblite");
-    }
+    // Link against and copy the CBL dynamic library:
+    let src = PathBuf::from(CBL_LIB_DIR).join(CBL_LIB_FILENAME);
+    let dst = out_dir.join(CBL_LIB_FILENAME);
+    println!("cargo:rerun-if-changed={}", src.to_str().unwrap());
+    fs::copy(src, dst).expect("copy dylib");
+    // Tell rustc to link it:
+    println!("cargo:rustc-link-search={}", out_dir.to_str().unwrap());
+    println!("cargo:rustc-link-lib=dylib=cblite");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=src/wrapper.h");
@@ -253,6 +174,4 @@ pub fn setup() {
         dest_path.join("libicuuc.so.63.1"),
     )
     .unwrap();
-    std::fs::copy(lib_path.join("cblite.dll"), dest_path.join("cblite.dll")).unwrap();
-    std::fs::copy(lib_path.join("cblite.lib"), dest_path.join("cblite.lib")).unwrap();
 }
