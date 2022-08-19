de68a890bb2e8a9bdf32fbce09d662108aa61a8c
MatthieuClochardDoctolib <matthieu.clochard@doctolib.com>
2022-08-19 15:14:01 +0200

    Use include headers for ios

--- build.rs
+++ build.rs
@@ -32,25 +32,6 @@ use std::path::PathBuf;
 static CBL_INCLUDE_DIR: &str = "libcblite-3.0.2/include";
 static CBL_LIB_DIR: &str = "libcblite-3.0.2/lib";
 
-fn wrapper_path() -> &'static str {
-    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
-    if target_os != "ios" {
-        "src/wrapper.h"
-    } else {
-        "src/wrapper_ios.h"
-    }
-}
-
-fn headers_dir() -> &'static str {
-    if env::var("TARGET").unwrap().ends_with("apple-ios") {
-        "libcblite-3.0.2/lib/aarch64-apple-ios/CouchbaseLite.xcframework/ios-arm64_armv7/CouchbaseLite.framework/Headers"
-    } else if env::var("TARGET").unwrap().ends_with("apple-ios-sim") {
-        "libcblite-3.0.2/lib/aarch64-apple-ios/CouchbaseLite.xcframework/ios-arm64_i386_x86_64-simulator/CouchbaseLite.framework/Headers"
-    } else {
-        CBL_INCLUDE_DIR
-    }
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     generate_bindings()?;
     configure_rustc()?;
@@ -61,8 +42,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 fn generate_bindings() -> Result<(), Box<dyn Error>> {
     let bindings = bindgen::Builder::default()
-        .header(wrapper_path())
-        .clang_arg(format!("-I{}", headers_dir()))
+        .header("src/wrapper.h")
+        .clang_arg(format!("-I{}", CBL_INCLUDE_DIR))
         .whitelist_type("CBL.*")
         .whitelist_type("FL.*")
         .whitelist_var("k?CBL.*")
--- src/wrapper_ios.h
+++
@@ -1,1 +0,0 @@
-#include "CouchbaseLite.h"
