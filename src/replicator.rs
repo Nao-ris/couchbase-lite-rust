32b3510763a979fa0ce168a604aa242fe17d0e63
Nao-ris <antoine.menciere@doctolib.com>
2023-03-14 16:10:53 +0100

    Replicator stop: make timeout a parameter

--- src/replicator.rs
+++ src/replicator.rs
@@ -597,9 +597,11 @@ impl Replicator {
 
     /** Stops a running replicator, asynchronously. Does nothing if it's not already started.
     The replicator will call your \ref CBLReplicatorChangeListener with an activity level of
-    \ref kCBLReplicatorStopped after it stops. Until then, consider it still active. */
-    pub fn stop(&mut self) -> bool {
+    \ref kCBLReplicatorStopped after it stops. Until then, consider it still active.
+    The parameter timout_seconds has a default value of 10. */
+    pub fn stop(&mut self, timeout_seconds: Option<u64>) -> bool {
         unsafe {
+            let timeout_seconds = timeout_seconds.unwrap_or(10);
             let (sender, receiver) = channel();
             let callback: ReplicatorChangeListener = Box::new(move |status| {
                 if status.activity == ReplicatorActivityLevel::Stopped {
@@ -616,7 +618,9 @@ impl Replicator {
             let mut success = true;
             if self.status().activity != ReplicatorActivityLevel::Stopped {
                 CBLReplicator_Stop(self.get_ref());
-                success = receiver.recv_timeout(Duration::from_secs(10)).is_ok();
+                success = receiver
+                    .recv_timeout(Duration::from_secs(timeout_seconds))
+                    .is_ok();
             }
             CBLListener_Remove(token);
             success
